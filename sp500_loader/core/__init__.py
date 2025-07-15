1d080d88f0707df661114379c120f112199e4aaa
Jan <jgruca@students.uni-mainz.de>
2025-07-15 09:19:35 +0200

    further cleanup

--- sp500_loader/core/__init__.py
+++
@@ -1,8 +0,0 @@
-# ===========================================
-# FILE 2: sp500_loader/core/__init__.py (UPDATED)
-# ===========================================
-
-# Core functionality
-from .loader import *
-from .splitting import *
-from .environment import PortfolioEnv, MinimalPortfolioEnv, create_env_from_loader
