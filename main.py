978864de58d46d68ab1575c9f38c4ccb2e705d4c
Jan <jgruca@students.uni-mainz.de>
2025-10-01 20:14:44 +0200

    debug

--- main.py
+++ main.py
@@ -596,7 +596,7 @@ def main():
     logger.info("Using CSV logging backend")
 
     # Generate all experiment configurations
-    experiments = generate_experiment_configs(num_seeds=3) # TODO 
+    experiments = generate_experiment_configs(num_seeds=5) # TODO 
     # Apply CLI overrides
     for exp in experiments:
         if args.exp_name:
